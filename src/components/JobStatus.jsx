2e7d3ed439478924b349971bbf101f0ce3a4db72
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-06-01 22:01:29 +0545

    Fixed bug: Title empty when localStorage is empty

--- src/components/JobStatus.jsx
+++ src/components/JobStatus.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 
 function JobStatus({title, getData}) {
 
-    let cardTitle="";
+    let cardTitle=title=="total"?"Total Job Entries":title;
     let total=0;
     const jobDataArray = getData();
     if(jobDataArray != null){
         if(title == "total"){
-            cardTitle = "Total Job Entries";
             total = jobDataArray.length;
         }
         else{
-            cardTitle = title;
             for(let jobData of jobDataArray){
                 if(jobData.jobStatus.toLowerCase() == title.toLowerCase()){
                     total+=1;
