151a050937983346a6fa577e0690c48621f9ed48
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-06-02 17:40:51 +0545

    Added AddBtn component and cleaned up the mess

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import {useState} from 'react'
 import JobCard from './components/JobCard'
 import JobStatus from './components/JobStatus';
-import { v4 as uuidv4 } from 'uuid';
+
 import Overview from './components/Overview';
+import AddBtn from './components/AddBtn';
 
 function App() {
 
   const storageKey = "jobEntries";
-  
   const [cardShown, setCardShown] = useState(false);
-  const [companyName, setCompanyName] = useState('');
-  const [position, setPosition] = useState('');
-  const [status, setStatus] = useState('');
-  const [dateApplied, setDateApplied] = useState(new Date());
-  const jobInfo = {
-    companyName, setCompanyName,
-    position, setPosition,
-    status, setStatus,
-    dateApplied, setDateApplied
-  }
 
   function showJobCard(){
       cardShown==true?setCardShown(false):setCardShown(true);
@@ -28,32 +18,14 @@ function App() {
     return JSON.parse(localStorage.getItem(storageKey));
   }
 
-  function prepData(){
-      const jobData = {
-        companyName:companyName,
-        jobPosition:position,
-        jobStatus:status,
-        date:dateApplied.toDateString(),
-        id: uuidv4()
-      }
-      return jobData;
-  }
-
-  function saveData(jobData){
-      if(localStorage.getItem(storageKey) == null){
-        localStorage.setItem(storageKey,JSON.stringify([]));
-      }
-      const currentEntries = JSON.parse(localStorage.getItem(storageKey));
-      localStorage.setItem(storageKey,JSON.stringify([...currentEntries,jobData]));
-      setCardShown(false);
-  }
-
   return (
     <>
     <div className="w-full flex justify-center">
-      <button className="p-4 border border-black bg-blue-400 text-3xl text-white rounded-2xl cursor-pointer" onClick={showJobCard}>Add</button>
+      <AddBtn showJobCard={showJobCard}/>
     </div>
-    {cardShown==true?<JobCard jobInfo={jobInfo} prepData={prepData} saveData={saveData}/>:null}
+    {cardShown==true?
+      <JobCard storageKey={storageKey} setCardShown={setCardShown} />:
+      null}
     
     <header className="w-full bg-gray-200 h-14">
       <nav className="h-full flex items-center">
