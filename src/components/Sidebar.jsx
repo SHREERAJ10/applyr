75ff400b2b4fcfbba4a5a129eb943e37891d2d52
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-06-06 21:39:02 +0545

    Modified code for routing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,38 +1,39 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 import { House, LogOut, BookText, ChartNoAxesColumnIncreasing, X } from 'lucide-react';
 
 function Sidebar({toggleSidebar}) {
   return (
     <>
-        <nav className="bg-white shadow-md text-zinc-900 w-full h-screen flex flex-col gap-4 p-4 rounded-r-2xl">
+        <nav className="bg-white shadow-md text-zinc-900 w-full flex flex-col gap-4 p-4 rounded-r-2xl h-screen">
           <div className="cursor-pointer" onClick={toggleSidebar}>
             <X />
           </div>
             <h2 className="text-2xl font-bold">Dashboard Menu</h2>
-            <ul className="text-xl font-semibold h-[60vh] flex flex-col gap-3 [&>*]:cursor-pointer [&>*]:p-4 [&>*]:rounded-2xl">
+            <ul className="text-xl font-semibold h-[60vh] flex flex-col gap-3 [&>*]:cursor-pointer [&>*]:rounded-2xl">
                 <li className="hover:bg-green-50">
-                  <div className="flex items-center gap-2">
+                  <NavLink to="/" className={({isActive})=>`w-full p-4 flex items-center gap-2 ${isActive?"bg-green-50":""}`}>
                     <House size={20} color="#34424E" />
                     <span>Dashboard</span>
-                  </div>
+                  </NavLink>
                 </li>
                 <li className="hover:bg-emerald-50">
-                  <div className="flex items-center gap-2">
+                  <NavLink to="/alljobs" className={({isActive})=>`w-full p-4 flex items-center gap-2 ${isActive?"bg-emerald-50":""}`}>
                     <BookText size={20} color="#4A8F8D" />
                     <span>All Jobs</span>
-                  </div>
+                  </NavLink>
                 </li>
                 <li className="hover:bg-lime-50">
-                  <div className="flex items-center gap-2">
+                  <NavLink to="/stats" className={({isActive})=>`w-full p-4 flex items-center gap-2 ${isActive?"bg-lime-50":""}`}>
                     <ChartNoAxesColumnIncreasing size={20} color="#228B55" />
                     <span>Stats</span>
-                  </div>
+                  </NavLink>
                 </li>
                 <li>
-                  <div className="flex items-center gap-2">
+                  <NavLink to="/logout" className="w-full p-4 flex items-center gap-2">
                     <LogOut size={20} color="#1A1C23" />
                     <span>Logout</span>
-                  </div>
+                  </NavLink>
                 </li>
             </ul>
         </nav>
