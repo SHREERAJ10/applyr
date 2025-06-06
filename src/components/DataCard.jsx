283b7ee1cad6d41976314ba131f322712873388c
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-06-06 07:10:46 +0545

    Created reusable card component showing job related data

---
+++ src/components/DataCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function DataCard({ getJobData, title }) {
+
+    const jobDataArray = getJobData().slice(0,6);
+
+    return (
+        <div className="bg-netural-50 border border-gray-100 shadow-sm rounded-xl w-1/2 px-4 py-2 flex flex-col gap-2">
+            <h2 className="text-lg font-semibold">{title}</h2>
+
+            <div className="flex flex-col gap-2">
+                    {jobDataArray.map((data) => {
+                        return (
+                            <div className="flex justify-between" key={data.id}>
+                                <li className="list-none">{data.position || data.companyName}</li>
+                                <span>{data.applications || ""}</span>
+                            </div>
+                        )
+                    })}
+            </div>
+
+        </div>
+    )
+}
+
+export default DataCard
