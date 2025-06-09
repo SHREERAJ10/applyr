9db8fc5a03fd41e6ecae21331985f6d3a694c47d
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-06-09 12:58:47 +0545

    Moved routing to App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import './index.css'
-import AppLayout from './components/AppLayout.jsx'
 import App from './App.jsx'
-import AllJobs from './pages/AllJobs.jsx'
-import Stats from './pages/Stats.jsx'
-import Logout from './pages/Logout.jsx'
+import './index.css'
 
-const router = createBrowserRouter([
-
-  {
-    path:'/',
-    element:<AppLayout />,
-    children:[
-      {
-        index:true,
-        element:<App />
-      },
-      {
-        path:'alljobs',
-        element:<AllJobs />
-      },
-      {
-        path:'stats',
-        element: <Stats />
-      },
-    ]
-  },
-  {
-    path:'logout',
-    element:<Logout />
-  }
-  
-  // {path:'/',element:<App />},
-  // {path:'/alljobs',element:<AllJobs />},
-  // {path:'/stats',element:<Stats />}
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
