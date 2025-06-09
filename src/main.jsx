10610049b68999a0f6b733e99b586863d57bacbe
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-06-09 09:07:39 +0545

    Implemented nested routing for Header Component

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
+import AppLayout from './components/AppLayout.jsx'
 import App from './App.jsx'
 import AllJobs from './pages/AllJobs.jsx'
 import Stats from './pages/Stats.jsx'
-import Logout from './pages/logout.jsx'
 
 const router = createBrowserRouter([
-  {path:'/',element:<App />},
-  {path:'/alljobs',element:<AllJobs />},
-  {path:'/stats',element:<Stats />},
-  {path:'/logout',element:<Logout />}
+
+  {
+    path:'/',
+    element:<AppLayout />,
+    children:[
+      {
+        index:true,
+        element:<App />
+      },
+      {
+        path:'alljobs',
+        element:<AllJobs />
+      },
+      {
+        path:'stats',
+        element: <Stats />
+      }
+    ]
+  }
+  
+  // {path:'/',element:<App />},
+  // {path:'/alljobs',element:<AllJobs />},
+  // {path:'/stats',element:<Stats />}
 ]);
 
 createRoot(document.getElementById('root')).render(
