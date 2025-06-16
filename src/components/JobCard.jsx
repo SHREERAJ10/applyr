57e094eef299b4ad4e651d8824d91e153ab05073
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-06-16 18:57:58 +0545

    Implemented view job card feature

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import Backdrop from './Backdrop';
+import Button from './Button';
 import { X, Calendar, Trash2, Pencil  } from 'lucide-react';
+import StatusTag from './statusTag';
 
-function JobCard() {
+function JobCard({cardData, toggleJobCard}) {
   return (
-    <div className="fixed top-56 w-full flex justify-center">
-        <div className="shadow shadow-black w-96 bg-white p-4 rounded-3xl flex flex-col gap-4">
-          <div className="flex justify-between">
+    <div className="fixed top-56 w-full flex justify-center p-4">
+        <Backdrop onClick={toggleJobCard} />
+        <div className="shadow shadow-black w-96 bg-white p-4 rounded-3xl flex flex-col gap-4 z-10">
+          <div className="flex justify-between items-start">
             <div>
-              <h1 className="font-bold text-2xl">Dropbox</h1>
-              <h2 className="font-semibold text-lg">Product Manager</h2>
+              <h1 className="font-bold text-2xl">{cardData.companyName}</h1>
+              <h2 className="font-semibold text-lg">{cardData.jobPosition}</h2>
             </div>
-            <div>
-              <X color="#9CA3AF" />
-            </div>
+            <Button onClick={toggleJobCard} text={<X color="#9CA3AF" />} /> 
           </div>
 
           <div className="flex justify-between">
@@ -20,17 +22,19 @@ function JobCard() {
               <Calendar color="#374151" strokeWidth={1.5} />
               <div className="flex flex-col text-gray-700">
                 <span>Applied on</span>
-                <span>June 6, 2024</span>
+                <span>{cardData.date}</span>
               </div>
             </div>
             <div>
-              <span>Interview</span>
+              <StatusTag status={cardData.jobStatus} />
             </div>
           </div>
 
           <div className="flex justify-between">
-            <Pencil color="	#3B82F6" strokeWidth={1.5} />
-            <Trash2 color="#EF4444" strokeWidth={1.5} />
+            <Button text={<Pencil color="	#3B82F6" strokeWidth={1.5} />} /> 
+            <Button text={<Trash2 color="#EF4444" strokeWidth={1.5} />} /> 
+            
+            
           </div>
         </div>
     </div>
