338c6ec42507e7f28128189b22a776c8453034d2
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-06-19 16:17:37 +0545

    Implemented delete feature

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -3,34 +3,52 @@ import JobsList from '../components/JobsList'
 import SortDropdown from '../components/SortDropdown'
 import FilterDropdown from '../components/FilterDropdown';
 import JobCard from '../components/JobCard';
+import ConfirmationPopup from '../components/ConfirmationPopup';
 
-function AllJobs({ getData }) {
+function AllJobs({ getData, storageKey }) {
 
-  const allJobData = getData();
+  const [allJobData,setAllJobData] = useState(getData());
   const [jobCardData, setJobCardData] = useState('');
   const [showJobCard, setShowJobCard] = useState(false);
   const [sortBy, setSortBy] = useState('latest');
   const [filterBy, setFilterBy] = useState('all');
+  const [deleteBtnPressed, setDeleteBtnPressed] = useState(false);
 
   function filterArray() {
     let filteredArray = filterBy == 'all' ? allJobData : 
     allJobData.filter((jobData) => {
-      if (jobData.jobStatus.toLowerCase() == filterBy) {
-        return jobData;
-      }
+      return jobData.jobStatus.toLowerCase() == filterBy
     })
     return filteredArray;
   }
 
   function sortArray(filteredArray) {
-    return sortBy=='oldest'?filteredArray:filteredArray.reverse();
+    return sortBy=='oldest'?filteredArray:[...filteredArray].reverse();
   }
+
+
 
   function toggleJobCard(){
     showJobCard==true?setShowJobCard(false):setShowJobCard(true);
-    document.body.classList.toggle("overflow-hidden")
+    document.body.classList.toggle("overflow-hidden");
   }
 
+  function proceedToDelete(){
+    deleteBtnPressed==true?setDeleteBtnPressed(false):setDeleteBtnPressed(true);
+    document.body.classList.toggle("overflow-hidden");
+    toggleJobCard();
+  }
+
+  function deleteJobEntry(jobId){
+    let newJobDataArray = allJobData.filter((jobEntry)=>{
+      if(jobEntry.id!=jobId){
+        return jobEntry;
+      }
+    });
+    localStorage.setItem(storageKey, JSON.stringify(newJobDataArray));
+    setAllJobData(newJobDataArray)
+    setDeleteBtnPressed(false);
+  }
 
   return (
     <div className="flex flex-col gap-2 pt-2">
@@ -41,9 +59,12 @@ function AllJobs({ getData }) {
 
       <JobsList jobEntries={sortArray(filterArray())} setJobCardData = {setJobCardData} toggleJobCard={toggleJobCard} />
 
-      {showJobCard==true?<JobCard cardData={jobCardData} toggleJobCard={toggleJobCard} />:null}
+      {showJobCard==true?<JobCard cardData={jobCardData} toggleJobCard={toggleJobCard} deleteOrNot={proceedToDelete} />:null}
 
-      
+      {deleteBtnPressed==true?
+        <ConfirmationPopup text="Are you sure you want to delete this?" deleteOrNot={proceedToDelete} deleteJobEntry={deleteJobEntry} curJobId = {jobCardData.id} />:
+        null  
+    }
 
     </div>
   )
