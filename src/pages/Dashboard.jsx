4c1917d1ee3ece571b29e8643af6b8f0f958825f
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-06-24 20:51:38 +0545

    Improved layout to match desktop view

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,14 @@
-import { useState, useContext } from "react";
+import { useState, useEffect } from "react";
 import JobEntryForm from "../components/JobEntryForm";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import Overview from "../components/Overview";
 import Button from "../components/Button";
-import Header from "../components/Header";
 import DataCard from "../components/DataCard";
 import JobsList from "../components/JobsList";
+import Backdrop from "../components/Backdrop"
 
-function Dashboard({getData}) {
+function Dashboard({ getData, storageKey }) {
 
-  const storageKey = "jobEntries";
   const [cardShown, setCardShown] = useState(false);
 
   function showJobCard() {
@@ -20,18 +19,19 @@ function Dashboard({getData}) {
     const jobDataArray = getData();
     let applicationsPerPosition = {};
 
-    if(jobDataArray.length!=0){
+    if (jobDataArray.length != 0) {
       for (let jobData of jobDataArray) {
         let position = jobData.jobPosition;
-        if (position.trim().toLowerCase() in applicationsPerPosition) {
-          applicationsPerPosition[position] += 1;
-        } else {
-          applicationsPerPosition[position] = 1;
+        if (position in applicationsPerPosition) {
+          applicationsPerPosition[`${position}`] += 1;
+        }
+        else {
+          applicationsPerPosition[`${position}`] = 1;
         }
       }
       return applicationsPerPosition; //returns object
     }
-    else{
+    else {
       return null;
     }
   }
@@ -39,12 +39,12 @@ function Dashboard({getData}) {
   function applicationsPerPositionToArray(applicationsPerPosition) {
     let jobPositionsArr = [];
 
-    if(applicationsPerPosition != null){
+    if (applicationsPerPosition != null) {
       for (let key in applicationsPerPosition) {
-          jobPositionsArr.push({
+        jobPositionsArr.push({
           position: key,
-          applications: applicationsPerPosition[key],
-          id:uuidv4()
+          applications: applicationsPerPosition[key], //No. of Applications for particular position
+          id: uuidv4()
         });
       }
     }
@@ -55,7 +55,7 @@ function Dashboard({getData}) {
     let jobDataArray = getData();
     let pendingApplications = [];
 
-    if(jobDataArray.length!=0){
+    if (jobDataArray.length != 0) {
       for (let jobData of jobDataArray) {
         if (jobData.jobStatus.toLowerCase() == "pending") {
           pendingApplications.push(jobData);
@@ -63,41 +63,59 @@ function Dashboard({getData}) {
       }
     }
     return pendingApplications; //returns array: empty or with elements
-    
+
   }
 
+  useEffect(() => {
+    if (cardShown) {
+      const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+      document.body.classList.add("overflow-hidden");
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+    }
+    else {
+      document.body.classList.remove("overflow-hidden");
+      document.body.style.paddingRight = "0px";
+    }
+  }, [cardShown]);
+
   return (
-    <>
-    <Overview getData={getData} />
+    <div className="pb-4">
+      <Overview getData={getData} />
 
       <div className="flex px-2 gap-2">
         <DataCard
           title="Jobs By Category"
-          getJobData={() =>{
+          getJobData={() => {
             return applicationsPerPositionToArray(getApplicationsPerPosition())
           }
           }
         />
+
         <DataCard title="Waiting to Hear From" getJobData={getPendingApplications} />
       </div>
-      
+
       <div className="mt-4">
         <h2 className="font-bold text-xl pl-8">Recent Activity</h2>
 
         {/* Passes only last 3 elements from the array */}
-        <JobsList jobEntries={(()=>getData().slice(-3).reverse())()} />
+        <JobsList jobEntries={(() => getData().slice(-3).reverse())()} />
       </div>
 
-      <div className="w-full sticky bottom-4 flex justify-end pr-4 z-0">
+      <div className="fixed bottom-4 right-4 lg:right-12 flex justify-end z-0">
         <div className="bg-blue-500 w-16 h-16 rounded-full text-white text-4xl flex justify-center items-center">
           <Button onClick={showJobCard} text="+" />
         </div>
       </div>
 
       {cardShown == true ? (
-        <JobEntryForm storageKey={storageKey} setCardShown={setCardShown} />
+        <div>
+            <Backdrop onClick={() => setCardShown(false)} />
+          <div className="fixed top-[50%] left-[50%] -translate-[50%] z-20">
+            <JobEntryForm setCardShown={setCardShown} storageKey={storageKey} />
+          </div>
+        </div>
       ) : null}
-    </>
+    </div>
   )
 }
 
