18a78cb3cf2124a011a66f8aec4a4514b5faf475
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-06-25 12:56:47 +0545

    Removed unwanted code

--- src/components/JobEntry.jsx
+++ src/components/JobEntry.jsx
@@ -3,14 +3,6 @@ import StatusTag from './StatusTag'
 
 function JobEntry({title, subtitle, status, onClick}) {
 
-  const tagColorObj = {
-    Interview:['bg-blue-100','text-blue-700'],
-    Offered:['bg-green-100','text-green-700'],
-    Wishlist:['bg-violet-100','text-violet-700'],
-    Pending:['bg-yellow-100','text-yellow-700'],
-    Rejected:['bg-red-100','text-red-700']
-  }
-
   return (
     <div className="w-[90%] lg:bg-neutral-50 flex items-center justify-between border-b-1 border-gray-400 p-2 cursor-pointer hover:bg-gray-50" onClick={onClick}>
         <div className="flex flex-col">
