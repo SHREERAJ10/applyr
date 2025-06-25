fd7975fe47e7bc6d0c7a39c02953016f0ae49279
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-06-25 11:29:11 +0545

    Corrected the routing of sidebar items to match with landing page setup

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,44 +1,43 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
-import { House, LogOut, BookText, ChartNoAxesColumnIncreasing, X } from 'lucide-react';
-
-function Sidebar({toggleSidebar}) {
+import { NavLink } from 'react-router-dom'
+import { House, LogOut, BookText, ChartNoAxesColumnIncreasing, X }
+ from 'lucide-react';
+function Sidebar({ toggleSidebar }) {
   return (
-    <>
-        <nav className="bg-white shadow-md text-zinc-900 w-full flex flex-col gap-4 p-4 rounded-r-2xl h-screen">
-          <div className="cursor-pointer" onClick={toggleSidebar}>
+    <div className="">
+        <nav className="bg-white text-zinc-900 w-full flex flex-col gap-4 p-4 h-screen">
+          <div className="cursor-pointer lg:hidden" onClick={toggleSidebar}>
             <X />
           </div>
-            <h2 className="text-2xl font-bold">Dashboard Menu</h2>
-            <ul className="text-xl font-semibold h-[60vh] flex flex-col gap-3 [&>*]:cursor-pointer [&>*]:rounded-2xl">
-                <li className="hover:bg-green-50">
-                  <NavLink to="/" className={({isActive})=>`w-full p-4 flex items-center gap-2 ${isActive?"bg-green-50":""}`}>
-                    <House size={20} color="#34424E" />
-                    <span>Dashboard</span>
-                  </NavLink>
-                </li>
-                <li className="hover:bg-emerald-50">
-                  <NavLink to="/alljobs" className={({isActive})=>`w-full p-4 flex items-center gap-2 ${isActive?"bg-emerald-50":""}`}>
-                    <BookText size={20} color="#4A8F8D" />
-                    <span>All Jobs</span>
-                  </NavLink>
-                </li>
-                <li className="hover:bg-lime-50">
-                  <NavLink to="/stats" className={({isActive})=>`w-full p-4 flex items-center gap-2 ${isActive?"bg-lime-50":""}`}>
-                    <ChartNoAxesColumnIncreasing size={20} color="#228B55" />
-                    <span>Stats</span>
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/logout" className="w-full p-4 flex items-center gap-2">
-                    <LogOut size={20} color="#1A1C23" />
-                    <span>Logout</span>
-                  </NavLink>
-                </li>
-            </ul>
+          <h2 className="text-2xl pl-6 lg:text-xl font-semibold">Applyr</h2>
+          <ul className="text-xl h-[60vh] flex flex-col gap-3 [&>*]:cursor-pointer [&>*]:rounded-2xl lg:text-lg font-medium">
+            <li className="hover:bg-green-50">
+              <NavLink to="/applyr/dashboard" className={({ isActive }) => `w-full p-4 flex items-center gap-2 rounded-2xl ${isActive ? "bg-green-50" : ""}`}>
+                <House size={20} color="#34424E" />
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li className="hover:bg-emerald-50">
+              <NavLink to="/applyr/alljobs" className={({ isActive }) => `w-full p-4 flex items-center gap-2 rounded-2xl ${isActive ? "bg-emerald-50" : ""}`}>
+                <BookText size={20} color="#4A8F8D" />
+                <span>All Jobs</span>
+              </NavLink>
+            </li>
+            <li className="hover:bg-lime-50">
+              <NavLink to="/applyr/stats" className={({ isActive }) => `w-full p-4 flex items-center gap-2 rounded-2xl ${isActive ? "bg-lime-50" : ""}`}>
+                <ChartNoAxesColumnIncreasing size={20} color="#228B55" />
+                <span>Stats</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/" className="w-full p-4 flex items-center gap-2">
+                <LogOut size={20} color="#1A1C23" />
+                <span>Logout</span>
+              </NavLink>
+            </li>
+          </ul>
         </nav>
-
-    </>
+    </div>
   )
 }
 
